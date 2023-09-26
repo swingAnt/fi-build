d39d9605bc79d15e6d310cdc29beec8ba82eb009
lijunkai7 <lijunkai7@jd.com>
2023-09-26 17:53:59 +0800

    fix

--- src/pages/Board/components/left.jsx
+++ src/pages/Board/components/left.jsx
@@ -5,7 +5,7 @@ const DragComponent = () => {
   const [isDragging, setIsDragging] = useState(false);
 
 
-  const handleDragStart = (type, e) => {
+  const handleDragStart = (key,type, e) => {
     setIsDragging(true);
     e.stopPropagation();
     e.dataTransfer.effectAllowed = "move";
@@ -18,25 +18,37 @@ const DragComponent = () => {
     //   console.log('offsetLeft',dragDom.offsetLeft)
       //后续替换成图/** 等组合，根据key区分大类
     //   draftEvent(e, "dragData", {key:'chart',type,lt:Math.floor(dragDom.offsetWidth/2),tp:dragDom.offsetHeight/2});
-    draftEvent(e, "dragData", {key:'chart',type,lt:0,tp:0});
+    draftEvent(e, "dragData", {key,type,lt:0,tp:0});
 
 };
   const handleDragEnd = () => {
     setIsDragging(false);
   };
 
-  return (
+  return (<>
     <div
       draggable
-      onDragStart={(e)=>handleDragStart('area_chart',e)}
+      onDragStart={(e)=>handleDragStart('chart','area_chart',e)}
       onDragEnd={handleDragEnd}
       style={{ width:'30px',opacity: isDragging ? 1 : 0.5 }}
       onDragOver={(e) => {
         e.preventDefault();
     }}
     >
-      Drag me!
+     图
     </div>
+    <div
+      draggable
+      onDragStart={(e)=>handleDragStart('row','area_chart',e)}
+      onDragEnd={handleDragEnd}
+      style={{ width:'30px',opacity: isDragging ? 1 : 0.5 }}
+      onDragOver={(e) => {
+        e.preventDefault();
+    }}
+    >
+     行
+    </div>
+    </>
   );
 };
 
